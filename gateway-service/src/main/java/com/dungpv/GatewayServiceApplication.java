2742520ecc5c8bb2baa408728b4138278a7b8524
dungpv <dungpv@itsol.vn>
2022-10-25 15:05:10 +0700

    dungpv commit

--- gateway-service/src/main/java/com/dungpv/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/dungpv/GatewayServiceApplication.java
@@ -1,54 +1,13 @@
 package com.dungpv;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class GatewayServiceApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(GatewayServiceApplication.class);
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
-	private static void logApplicationStartup(Environment env) {
-		String serverPort = env.getProperty("server.port");
-		String contextPath = env.getProperty("server.servlet.context-path");
-		if (StringUtils.isBlank(contextPath)) {
-			contextPath = "/";
-		}
-		String hostAddress = "localhost";
-		try {
-			hostAddress = InetAddress.getLocalHost().getHostAddress();
-		} catch (UnknownHostException e) {
-			log.warn("The host name could not be determined, using `localhost` as fallback");
-		}
-		log.info("\n------------\n\t" +
-						"Application '{}' is running! Access URLs:\n\t" +
-						"//localhost:{}{}\n\t" +
-						"External: \t{}\n\t" +
-						"Profile(s): \t{}\n----------------------------------------------------------",
-				env.getProperty("spring.application.name"),
-				serverPort,
-				contextPath,
-				hostAddress,
-				serverPort,
-				env.getActiveProfiles());
-
-		String configServerStatus = env.getProperty("configserver.status");
-		if (configServerStatus == null) {
-			configServerStatus = "Not found or not setup for this application";
-		}
-		log.info("\n----------------------------------------------------------\n\t" +
-				"Config Server: \t{}\n----------------------------------------------------------", configServerStatus);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
